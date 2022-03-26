e44becf8a0d40546866d8dd6200c1d2d528d8e7e
iamrealfarhanbd <iamfarhan09@gmail.com>
2022-03-26 21:10:50 +0600

    accordian added

--- src/components/QnA/QnA.js
+++ src/components/QnA/QnA.js
@@ -4,14 +4,14 @@ const QnA = (props) => {
   const   {question,answer} = props.qnA
     return (
         <>
-            <div className="accordion pt-5" id="accordionExample">
+            <div className="accordion pt-5" >
             <div className="accordion-item">
                 <h2 className="accordion-header" id="headingOne">
-                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                <button className="accordion-button">
                    <h4>{question}</h4>
                 </button>
                 </h2>
-                <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div id="" className="accordion-collapse collapse show">
                     <div className="accordion-body">
                         <p >{answer}</p>
                     </div>
